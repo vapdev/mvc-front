5d00dd315ed90470d6205f988cbe27dc6bd360b2
vitor <vpbohn@gmail.com>
2023-10-06 04:10:18 -0300

    build: railway config

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL = 'http://mvc-api.railway.internal/api';
--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../config.js'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     }
